860b353a6877cd30d632ccd607b5bbd2785bc58d
Dave Germiquet <davegermiquet@trulycanadian.net>
2020-03-01 13:05:16 -0500

    Add override for parse request after finding more issues
    This has more issues, with timeouts relocation when getting the following response 
    
    _ Relocated ..click on the following link

--- respmod_copy.py
+++ respmod_copy.py
@@ -71,6 +71,99 @@ class ICAPHandler(BaseICAPRequestHandler):
             print(content)
             self.write_chunk(content)
             
+    def parse_request(self):
+        """Parse a request (internal).
+        The request should be stored in self.raw_requestline; the results
+        are in self.command, self.request_uri, self.request_version and
+        self.headers.
+        Return True for success, False for failure; on failure, an
+        error is sent back.
+        """
+        self.command = None
+        self.request_version = version = 'ICAP/1.0'
+
+        # Default behavior is to leave connection open
+        self.close_connection = False
+
+        requestline = self.raw_requestline.rstrip(b'\r\n')
+        self.requestline = requestline
+
+        words = requestline.split()
+        if len(words) != 3:
+            raise ICAPError(400, "Bad request syntax (%r)" % requestline)
+
+        command, request_uri, version = words
+
+        if version[:5] != b'ICAP/':
+            raise ICAPError(400, "Bad request protocol, only accepting ICAP")
+
+        if command not in (b'OPTIONS', b'REQMOD', b'RESPMOD'):
+            raise ICAPError(501, "command %r is not implemented" % command)
+
+        try:
+            base_version_number = version.split(b'/', 1)[1]
+            version_number = base_version_number.split(b".")
+            # RFC 2145 section 3.1 says there can be only one "." and
+            #   - major and minor numbers MUST be treated as
+            #      separate integers;
+            #   - ICAP/2.4 is a lower version than ICAP/2.13, which in
+            #      turn is lower than ICAP/12.3;
+            #   - Leading zeros MUST be ignored by recipients.
+            if len(version_number) != 2:
+                raise ValueError
+            version_number = int(version_number[0]), int(version_number[1])
+        except (ValueError, IndexError):
+            raise ICAPError(400, "Bad request version (%r)" % version)
+
+        if version_number != (1, 0):
+            raise ICAPError(
+                505, "Invalid ICAP Version (%s)" % base_version_number
+            )
+
+        self.command, self.request_uri, self.request_version = \
+            command, request_uri, version
+
+        # Examine the headers and look for a Connection directive
+        self.headers = self._read_headers()
+
+        conntype = self.headers.get(b'connection', [b''])[0]
+        if conntype.lower() == b'close':
+            self.close_connection = True
+
+        self.encapsulated = {}
+        if self.command in [b'RESPMOD', b'REQMOD']:
+            for enc in self.headers.get(b'encapsulated', [b''])[0].split(b','):
+                # TODO: raise ICAPError if Encapsulated is malformed or empty
+                k, v = enc.strip().split(b'=')
+                self.encapsulated[k] = int(v)
+
+        self.preview = self.headers.get(b'preview', [None])[0]
+        self.allow = [
+            x.strip() for x in self.headers.get(b'allow', [b''])[0].split(b',')
+        ]
+        self.client_ip = self.headers.get(
+            b'x-client-ip', b'No X-Client-IP header')[0]
+
+        if self.command == b'REQMOD':
+            if b'req-hdr' in self.encapsulated:
+                self.enc_req = self._read_request()
+                self.enc_req_headers = self._read_headers()
+            if b'req-body' in self.encapsulated:
+                self.has_body = True
+        elif self.command == b'RESPMOD':
+            if b'req-hdr' in self.encapsulated:
+                self.enc_req = self._read_request()
+                self.enc_req_headers = self._read_headers()
+            if b'res-hdr' in self.encapsulated:
+                self.enc_res_status = self._read_status()
+                self.enc_res_headers = self._read_headers()
+            if b'res-body' in self.encapsulated:
+                self.has_body = True
+        # Else: OPTIONS. No encapsulation.
+
+        # Parse service name
+        # TODO: document "url routing"
+        self.servicename = urlparse(self.request_uri)[2].strip(b'/')
             
     def handle_one_request(self):
         """Handle a single HTTP request.
